56823c2ed510e3e01379e6b146cebef912f9c984
Gustavo Martins <tioben@outlook.com.br>
2023-06-25 11:34:11 -0300

    25/06/2023

--- Fundamentos/POO/iterators/iterator1.py
+++ Fundamentos/POO/iterators/iterator1.py
@@ -1,6 +1,6 @@
 myTuple = ("apple", "banana", "cherry")
 myTupleIter = iter(myTuple)
-print(next(myTupleIter))
-print(next(myTupleIter))
-print(next(myTupleIter))
+print(next(myTupleIter)) #apple
+print(next(myTupleIter)) #banana
+print(next(myTupleIter)) #cherry
 print(next(myTupleIter)) #error
