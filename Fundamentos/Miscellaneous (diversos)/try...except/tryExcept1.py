e8f41692e0220f01c529915aa5b6910b4a9c59ae
Gustavo Martins <tioben@outlook.com.br>
2023-06-28 13:44:35 -0300

    28/06/2023

--- Fundamentos/Miscellaneous (diversos)/try...except/tryExcept1.py
+++ Fundamentos/Miscellaneous (diversos)/try...except/tryExcept1.py
@@ -10,3 +10,24 @@ try:
 except:
     print("Error!")
 
+try:
+    print(h)
+except NameError: #especificando o erro
+    print(f"Variable is not defined!")
+else:
+    print("Incomum error!")
+finally:
+    print("The try test finish!")
+
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+
+
